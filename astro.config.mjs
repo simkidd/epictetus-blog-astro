6d593db2dd3a334bf660614605a8bc334f033822
John Mason <87030324+simkidd@users.noreply.github.com>
2024-01-09 10:00:44 +0100

    chore: 404 page added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
+  integrations: [icon()]
 });
